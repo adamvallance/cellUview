409268182696f759c517d6781c8191016310f7de
Adam Vallance <adamvallance18@gmail.com>
2023-02-15 23:01:22 +0000

    Sketched out header file for camera object which holds stream. Still to add ability to view a passthrough.

---
+++ camera.h
@@ -0,0 +1,38 @@
+/*camera.h
+    created: 15/02/23
+    last updated: 15/02/23
+    author: adamvallance
+    description: contains class declaration for camera class
+     which creates a stream from the pi camera 
+    optionally creates a websocket connection for the app
+*/
+
+#ifndef CAMERA_H_
+#define CAMERA_H_
+#include <raspicam/raspicam.h> //potentially update this to use libcamera 2 and later version of raspbian
+#include <opencv2/opencv.hpp> //for displaying stream?
+
+
+
+namespace std {
+
+class camera {
+public:
+	camera();
+	virtual ~camera();
+
+    void start_stream();
+    void stop_stream();
+
+private:
+	raspicam::RaspiCam camera_;
+    void callbackTBD(); //needs worked out
+
+};
+
+} /* namespace std */
+
+
+
+#endif
+/* CAMERA_H_ */
