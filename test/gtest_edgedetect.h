5e401bf7abf6a617a2183af796d14625cb41eab5
Malik <2361424s@student.gla.ac.uk>
2023-03-21 13:48:58 +0000

    Expanded test to check threshold value

--- test/gtest_edgedetect.h
+++ test/gtest_edgedetect.h
@@ -23,10 +23,29 @@ TEST(edgeDetectTest, checkOutputNote){
     capture.create(5, 5, 1);    //random matrix to process, output not used in test
     f.image = capture;
     f.note = "";
-    // POSSIBLE IMPROVEMENT: expand frame struct to include dedicated testnote aside from note?
 
     edge.registerCallback(&endtest);
     edge.newFrame(f);
 
     EXPECT_EQ(endtest.currentFrame.note, expected.note);
+}
+
+TEST(edgeDetectTest, checkThresholdMetadata){
+    
+    EndTester endtest;
+    edgeDetection edge;
+
+    imageProcessor::frame expected; //expected output frame
+    expected.edgeThreshold = 0.9;   //default value
+    // expected.edgeThreshold = 0;     //debug, using this will cause a fail
+    
+    imageProcessor::frame f;
+    cv::Mat capture;  
+    capture.create(5, 5, 1);    //random matrix to process, output not used in test
+    f.image = capture;
+
+    edge.registerCallback(&endtest);
+    edge.newFrame(f);
+
+    EXPECT_EQ(endtest.currentFrame.edgeThreshold, expected.edgeThreshold);
 }
