f46315417a47012d7e5c1b7fe093cf300837639d
Malik <2361424s@student.gla.ac.uk>
2023-03-21 14:06:37 +0000

    Removing fixed note test

--- test/gtest_edgedetect.h
+++ test/gtest_edgedetect.h
@@ -7,29 +7,6 @@
 #include <iostream>
 
 
-TEST(edgeDetectTest, checkOutputNote){
-    
-    EndTester endtest;
-    edgeDetection edge;
-
-    imageProcessor::frame expected; //expected output frame
-    // cv::Mat cap;      
-    // expected.image = cap;
-    expected.note = "Frame processed through edge detection"; //note from running through edge detection, must match edgeDetection.cpp
-    // expected.note = "This is a dummy note"; //debug, using this note will cause a fail
-    
-    imageProcessor::frame f;
-    cv::Mat capture;  
-    capture.create(5, 5, 1);    //random matrix to process, output not used in test
-    f.image = capture;
-    f.note = "";
-
-    edge.registerCallback(&endtest);
-    edge.newFrame(f);
-
-    EXPECT_EQ(endtest.currentFrame.note, expected.note);
-}
-
 TEST(edgeDetectTest, checkThresholdMetadata){
     
     EndTester endtest;
