ddc0e82eb2369d5371dfc5c26a35e832fbe28cba
Malik <2361424s@student.gla.ac.uk>
2023-04-15 13:16:12 +0100

    Initial docs gen with customised doxygen-awesome style

---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['camera_0',['Camera',['../class_camera.html',1,'']]],
+  ['celluviewwelcome_1',['cellUviewWelcome',['../classcell_uview_welcome.html',1,'']]],
+  ['contrastenhancement_2',['contrastEnhancement',['../classcontrast_enhancement.html',1,'']]]
+];
