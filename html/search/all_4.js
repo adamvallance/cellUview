8b3f16e766e30c99642e94f8cc059fe335855438
adamvallance <adamvallance@users.noreply.github.com>
2023-04-15 20:18:18 +0000

    deploy: f2baa40fee98e3c38b25e9b03a4dd4b92bed677a

---
+++ html/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['gallery_0',['Gallery',['../class_gallery.html',1,'']]],
+  ['grayscale_1',['grayScale',['../classgray_scale.html',1,'']]],
+  ['gui_2',['Gui',['../class_gui.html',1,'Gui'],['../class_gui.html#a0787d4633eeebebc31ec40ad2b441c25',1,'Gui::Gui()']]]
+];
