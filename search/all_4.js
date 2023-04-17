a79cd88279fb4b95c7edaca88b22097a792db3aa
adamvallance <adamvallance@users.noreply.github.com>
2023-04-17 00:25:49 +0000

    deploy: 56ab13efe55c9769365bf39d0c05be50a92121fd

--- search/all_4.js
+++ search/all_4.js
@@ -1,13 +1,10 @@
 var searchData=
 [
   ['gallery_0',['Gallery',['../class_gallery.html',1,'']]],
-  ['getconnected_1',['getConnected',['../class_motor_driver.html#a8f129145fed71a097b722afdcf0fcdfa',1,'MotorDriver']]],
-  ['getison_2',['getIsOn',['../class_camera.html#a83396433ca6457173dc1844152715de2',1,'Camera']]],
-  ['getparam_3',['getParam',['../classframe.html#af9c18f99fb0ec5174a320c7bd2601b83',1,'frame']]],
-  ['getparamlabel_4',['getParamLabel',['../classcontrast_enhancement.html#a08c202cd0a1d9dbd7974b6273f0f6ade',1,'contrastEnhancement::getParamLabel()'],['../classdilation.html#aac678c6a5fd8a4bcd6e5fd82a4ba9769',1,'dilation::getParamLabel()'],['../classedge_detection.html#af479eae661c2bd12f3bee621a97e15b9',1,'edgeDetection::getParamLabel()'],['../classerosion.html#a3fa83e64a15e8f6eefede9d3831c3d41',1,'erosion::getParamLabel()'],['../classflat_field_correct.html#a263a8f8f9c0172aa0eed261499510b08',1,'flatFieldCorrect::getParamLabel()'],['../classgray_scale.html#a55a475e399534ec4e5467c24f0503c2b',1,'grayScale::getParamLabel()']]],
-  ['getparamsize_5',['getParamSize',['../classframe.html#aea4d8c6ae44c207aa6e7130c2abe8e85',1,'frame']]],
-  ['getposition_6',['getPosition',['../class_motor_driver.html#aef9fa0379576ad5af9da4d3a6622b1d3',1,'MotorDriver']]],
-  ['getrunning_7',['getRunning',['../class_motor_driver.html#aac66fbe16fd9d4d5a5aa40f8c8d9f9c1',1,'MotorDriver']]],
-  ['grayscale_8',['grayScale',['../classgray_scale.html',1,'']]],
-  ['gui_9',['Gui',['../class_gui.html',1,'Gui'],['../class_gui.html#a7daf51f508a0ff0f4259548d9f0349ea',1,'Gui::Gui()']]]
+  ['getison_1',['getIsOn',['../class_camera.html#a83396433ca6457173dc1844152715de2',1,'Camera']]],
+  ['getparam_2',['getParam',['../classframe.html#af9c18f99fb0ec5174a320c7bd2601b83',1,'frame']]],
+  ['getparamlabel_3',['getParamLabel',['../classcontrast_enhancement.html#a08c202cd0a1d9dbd7974b6273f0f6ade',1,'contrastEnhancement::getParamLabel()'],['../classdilation.html#aac678c6a5fd8a4bcd6e5fd82a4ba9769',1,'dilation::getParamLabel()'],['../classedge_detection.html#af479eae661c2bd12f3bee621a97e15b9',1,'edgeDetection::getParamLabel()'],['../classerosion.html#a3fa83e64a15e8f6eefede9d3831c3d41',1,'erosion::getParamLabel()'],['../classflat_field_correct.html#a263a8f8f9c0172aa0eed261499510b08',1,'flatFieldCorrect::getParamLabel()'],['../classgray_scale.html#a55a475e399534ec4e5467c24f0503c2b',1,'grayScale::getParamLabel()']]],
+  ['getparamsize_4',['getParamSize',['../classframe.html#aea4d8c6ae44c207aa6e7130c2abe8e85',1,'frame']]],
+  ['grayscale_5',['grayScale',['../classgray_scale.html',1,'']]],
+  ['gui_6',['Gui',['../class_gui.html',1,'Gui'],['../class_gui.html#a0787d4633eeebebc31ec40ad2b441c25',1,'Gui::Gui()']]]
 ];
