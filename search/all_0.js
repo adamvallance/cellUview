52f926eda6d22d11fc61ee4c3fba16d103443b12
adamvallance <adamvallance@users.noreply.github.com>
2023-04-17 14:04:53 +0000

    deploy: f2fddcdb7eb3355446e46c7fab9804c8be687d9b

--- search/all_0.js
+++ search/all_0.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['camera_0',['Camera',['../class_camera.html',1,'']]],
-  ['capturemetadata_1',['captureMetadata',['../class_camera.html#a974535be48b49903ad17d3fd456c6b1c',1,'Camera']]],
-  ['celluviewwelcome_2',['cellUviewWelcome',['../classcell_uview_welcome.html',1,'']]],
-  ['contrastenhancement_3',['contrastEnhancement',['../classcontrast_enhancement.html',1,'']]],
-  ['copyfrom_4',['copyFrom',['../classframe.html#a7f497befd5e8e84ad7e3f67e7b1ce0d5',1,'frame']]]
+  ['captureframe_1',['captureFrame',['../class_gallery.html#a72a65b916b7fb06b753a69c404e66987',1,'Gallery']]],
+  ['capturemetadata_2',['captureMetadata',['../class_camera.html#a974535be48b49903ad17d3fd456c6b1c',1,'Camera']]],
+  ['celluviewwelcome_3',['cellUviewWelcome',['../classcell_uview_welcome.html',1,'']]],
+  ['contrastenhancement_4',['contrastEnhancement',['../classcontrast_enhancement.html',1,'']]],
+  ['copyfrom_5',['copyFrom',['../classframe.html#a7f497befd5e8e84ad7e3f67e7b1ce0d5',1,'frame']]]
 ];
--- search/classes_6.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['kmeanscluster_0',['kMeansCluster',['../classk_means_cluster.html',1,'']]]
-];
