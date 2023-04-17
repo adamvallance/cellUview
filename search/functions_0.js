ccc80e80fe57808b15844e4dedfd19c0f34e3177
mlsalloum <mlsalloum@users.noreply.github.com>
2023-04-17 13:53:01 +0000

    deploy: 2a543b0c449b5ba3ad2e7ff1b5a652774e352a3f

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['captureframe_0',['captureFrame',['../class_gallery.html#a72a65b916b7fb06b753a69c404e66987',1,'Gallery']]],
-  ['capturemetadata_1',['captureMetadata',['../class_camera.html#a974535be48b49903ad17d3fd456c6b1c',1,'Camera']]],
-  ['copyfrom_2',['copyFrom',['../classframe.html#a7f497befd5e8e84ad7e3f67e7b1ce0d5',1,'frame']]]
+  ['capturemetadata_0',['captureMetadata',['../class_camera.html#a974535be48b49903ad17d3fd456c6b1c',1,'Camera']]],
+  ['copyfrom_1',['copyFrom',['../classframe.html#a7f497befd5e8e84ad7e3f67e7b1ce0d5',1,'frame']]]
 ];
